ae8935186980a0fc924cb5d2b9749f77edc9141c
Andrew Wei <andrewscwei@gmail.com>
2018-03-04 15:16:20 -0800

    Polish tests

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,3 @@
-const assert = require(`assert`);
 const cssnano = require(`cssnano`);
 const fs = require(`fs`);
 const path = require(`path`);
@@ -6,13 +5,31 @@ const postcss = require(`postcss`);
 const postlude = require(`../`);
 const { describe, it } = require(`mocha`);
 
+const TEST_DIR = path.join(__dirname, `files`);
+
+async function compare(funcName) {
+  const pcss = fs.readFileSync(path.join(TEST_DIR, `${funcName}.pcss`), `utf8`);
+  const css = fs.readFileSync(path.join(TEST_DIR, `${funcName}.css`), `utf8`);
+  const src = await postcss([cssnano]).process(css, { from: undefined });
+  const dist = await postcss([postlude, cssnano]).process(pcss, { from : undefined });
+
+  if (src.css !== dist.css) {
+    const message = `Unexpected post-processed results\n       Expectation: ${src.css}\n           Reality: ${dist.css}`;
+    throw new Error(message);
+  }
+}
+
 describe(`postlude`, function() {
-  it(`margin`, async function() {
-    const pcss = fs.readFileSync(path.join(__dirname, `files`, `margin.pcss`), `utf8`);
-    const css = fs.readFileSync(path.join(__dirname, `files`, `margin.css`), `utf8`);
-    const src = await postcss([cssnano]).process(css, { from: undefined });
-    const dist = await postcss([postlude, cssnano]).process(pcss, { from : undefined });
-    assert(src.css === dist.css);
+  const funcNames = fs.readdirSync(TEST_DIR).reduce((arr, val) => {
+    if (!val.endsWith(`.pcss`)) return arr;
+    arr.push(path.basename(val, `.pcss`));
+    return arr;
+  }, []);
+
+  funcNames.forEach(funcName => {
+    it(funcName, async function() {
+      await compare(funcName);
+    });
   });
 });
 
