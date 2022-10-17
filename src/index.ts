69c4c3f3873e620cb2eb04a94f65c8c5fcf06665
Andrew Wei <andrewscwei@gmail.com>
2022-10-17 11:03:25 -0700

    Add copyright

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/** @license postlude
+ * © Andrew Wei
+ * This source code is licensed under the MIT license found in the LICENSE file in the root
+ * directory of this source tree.
+ */
+
 /* eslint-disable @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires */
 
 import useDebug from 'debug'
