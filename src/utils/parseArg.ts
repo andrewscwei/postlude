448cfeb32522c109d6e197c8837d92927b90e0d8
Andrew Wei <andrewscwei@gmail.com>
2022-10-25 01:01:42 -0700

    Add documentation to parseArgs

--- src/utils/parseArg.ts
+++ src/utils/parseArg.ts
@@ -1,6 +1,26 @@
 import assert from 'assert'
 
+/**
+ * Parses a list of arguments, returns the first matched argument that is whitelisted. If no matched
+ * argument is found, `undefined` is returned.
+ *
+ * @param args - A list of string arguments.
+ * @param whitelist - A list of whitelisted arguments.
+ *
+ * @returns The matched argument, else `undefined`.
+ */
 function parseArg(args: string[], whitelist: string[]): string | undefined
+
+/**
+ * Parses a list of arguments, returns the first matched argument that is whitelisted. If no matched
+ * argument is found, an optional default value is returned, otherwise `undefined` is returned.
+ *
+ * @param args - A list of string arguments.
+ * @param whitelist - A list of whitelisted arguments.
+ * @param defaultValue - The default value to return if a matched argument cannot be found.
+ *
+ * @returns The matched argument, else the default value, else `undefined`.
+ */
 function parseArg(args: string[], whitelist: string[], defaultValue: string): string
 function parseArg(args: string[], whitelist: string[], defaultValue?: string): string | undefined {
   if (whitelist.length <= 0) return defaultValue
